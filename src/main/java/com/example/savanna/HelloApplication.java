94a3d89680c231f6c7685679730d0c0e3c1a02e5
bryantan99 <tanjiaqin99@gmail.com>
2021-12-28 01:35:21 +0800

    - Add gitignore to ignore target folder - Add icon

--- src/main/java/com/example/savanna/HelloApplication.java
+++ src/main/java/com/example/savanna/HelloApplication.java
@@ -3,16 +3,30 @@ package com.example.savanna;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        try {
+            URL url = getClass().getResource("icon.png");
+            if (url != null) {
+                Image appIcon = new Image(url.toURI().toString());
+                stage.getIcons().add(appIcon);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        stage.setTitle("Savanna");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
